9b3dd67c0fcde487887d80616e9ce5e18cfed385
Stephany Eloise <stephany.eloise@hotmail.com>
2022-07-02 21:24:58 -0300

    Baloes de Fala, Morte

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 (function() {
   'use strict';
-
-  let pet = {
-    fome: 100,
-    higiene: 100, 
-    sono: 100, 
-    diversao: 100, 
-    social: 100
-  }; 
-
+ 
   const imagesUrls = {
     petRainbow: "img/rainbow.jpg",
     petAaa: "img/aaa.jpg",
@@ -29,13 +21,21 @@
   };
 
   const petImg = document.getElementById('pet');
+
+  let pet = {
+    fome: 100,
+    higiene: 100, 
+    sono: 100, 
+    diversao: 100, 
+    social: 100
+  };
   
   let status = { 
     fome: {
       taxa: 3,
       campo: document.getElementById('fome'),
       botao: document.getElementById('alimentar'),
-      tempo: 4000,
+      tempo: 400,
       temporizador: null,
       update: null
     },
@@ -43,7 +43,7 @@
       taxa: 2,
       campo: document.getElementById('higiene'),
       botao: document.getElementById('banho'),
-      tempo: 4000,
+      tempo: 400,
       temporizador: null,
       update: null
     },
@@ -51,7 +51,7 @@
       taxa: 4,
       campo: document.getElementById('sono'),
       botao: document.getElementById('dormir'),
-      tempo: 6000,
+      tempo: 600,
       temporizador: null,
       update: null
     },
@@ -59,7 +59,7 @@
       taxa: 2,
       campo: document.getElementById('diversao'),
       botao: document.getElementById('brincar'),
-      tempo: 1000,
+      tempo: 200,
       temporizador: null,
       update: null
     },
@@ -67,10 +67,15 @@
       taxa: 3,
       campo: document.getElementById('social'),
       botao: document.getElementById('socializar'),
-      tempo: 3000,
+      tempo: 300,
       temporizador: null,
       update: null
     }
+  };
+
+  let verify = {
+    tempo: 100,
+    temporizador: null
   };
 
   start()
@@ -81,6 +86,7 @@
     status.sono.temporizador = setInterval(sonoCiclo, status.sono.tempo);
     status.diversao.temporizador = setInterval(diversaoCiclo, status.diversao.tempo);
     status.social.temporizador = setInterval(socialCiclo, status.social.tempo);
+    verify.temporizador = setInterval(updateVerify, verify.tempo);
     addEventsToButtons();
   }; 
 
@@ -89,6 +95,28 @@
       pet[propriedade] = 0;
     status[propriedade].campo.innerHTML = pet[propriedade] + '%'; 
     status[propriedade].campo.style.width = pet[propriedade] + 'px'; 
+  };
+
+  function updateVerify() {
+    if(pet.fome <= 0 && pet.higiene <= 0 && pet.sono <= 0 && pet.diversao <= 0 && pet.social <= 0) {
+      document.getElementById("balaofala").textContent="Boo!";
+      return petImg.src=imagesUrls.petDeath;
+    } else if(pet.fome <= 0) {
+      petImg.src=imagesUrls.petGiveMeFood;
+      document.getElementById("balaofala").textContent="Me dê comida!";
+    } else if(pet.higiene <= 0) {
+      petImg.src=imagesUrls.petTrash;
+      document.getElementById("balaofala").textContent="Banho?";
+    } else if(pet.sono <= 0) {
+      petImg.src=imagesUrls.petTired;
+      document.getElementById("balaofala").textContent="Estou cansadito";
+    } else if(pet.diversao <= 0) {
+      petImg.src=imagesUrls.petBoring;
+      document.getElementById("balaofala").textContent="Boooring, arg";
+    } else if(pet.social <= 0) { 
+      petImg.src=imagesUrls.petPls;
+      document.getElementById("balaofala").textContent="ME AME!!!";
+    };
   };
 
   function fomeCiclo() {
@@ -116,35 +144,39 @@
     updateStatus('social');
   };
 
-
   function alimentar() {
     pet.fome = 100;
     updateStatus('fome');
     petImg.src=imagesUrls.petEat;
+    document.getElementById("balaofala").textContent="Hmmmm *-*";
   };
 
   function banho() {
     pet.higiene = 100;
     updateStatus('higiene');
     petImg.src=imagesUrls.petBath;
+    document.getElementById("balaofala").textContent="Quack quack";
   };
 
   function dormir() {
     pet.sono = 100;
     updateStatus('sono');
     petImg.src=imagesUrls.petSleepTime;
+    document.getElementById("balaofala").textContent="zzZ";
   };
 
   function brincar() {
     pet.diversao = 100;
     updateStatus('diversao');
     petImg.src=imagesUrls.petPlay;
+    document.getElementById("balaofala").textContent="Headshot, otário!";
   };
 
   function socializar() {
     pet.social = 100;
     updateStatus('social');
     petImg.src=imagesUrls.petPat;
+    document.getElementById("balaofala").textContent="Nya";
   };
 
   function addEventsToButtons() {
@@ -154,5 +186,5 @@
     status.diversao.botao.addEventListener('click', brincar);
     status.social.botao.addEventListener('click', socializar);
   };
-
+ 
 })();
